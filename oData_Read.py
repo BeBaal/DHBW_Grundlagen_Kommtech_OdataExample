9d8b30a07d8211e4b6c2aeedc506b3348f294c4f
BeBaal <86570412+BeBaal@users.noreply.github.com>
2022-07-07 21:48:08 +0200

    Delete oData_Read.py

--- oData_Read.py
+++
@@ -1,9 +0,0 @@
-import http.client
-
-conn = http.client.HTTPSConnection("services.odata.org")
-payload = ''
-headers = {}
-conn.request("GET", "/V4/TripPinService/(S(wfosoe1agrmphpx05u113cct))/People('maxmustermann')", payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
